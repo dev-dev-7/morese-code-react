bfb822529ff0b3e9e45fe08e8b8922e38a5e13f2
Ishro Dev <dev@ishro.com>
2022-07-25 16:50:22 +0400

    B UG FIX

--- src/App.js
+++ src/App.js
@@ -1,84 +1,142 @@
 import React, { useEffect, useRef, useState } from "react";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 function App() {
   const commandInput = useRef(null);
   const [show, setShow] = useState(false);
-  const [notFound, setNotFound] = useState(false);
   const [askCommand, setAskCommand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [tempMessage, setTempMessage] = useState("");
+
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setNotFound(true);
+    if (event.key === "Enter") {
+      messages.push({
+        key: event.target.value,
+        message: "Command not found. Type 'help' to see list of commands.",
+      });
+      let updated = messages;
+      setMessages(updated);
+      setTempMessage("Command not found. Type 'help' to see list of commands.");
     }
-  }
+  };
 
   useEffect(() => {
     if (commandInput.current) {
       commandInput.current.focus();
     }
   }, [show]);
-  return (    
+
+  console.log("messages: ", messages);
+
+  return (
     <div id="console">
-    <h1 className="glitch">Fvck F!AT</h1>
-    <h2 id="timer">Go to mint.fvckfiat.com</h2>
-    <Typewriter
-     options={{
-      delay:1, cursor:''
-    }}
-      onInit={(typewriter) => {
-      typewriter.typeString("<span id='consoleText'>Welcome! Type 'help' to see list of commands. </span>")
-      .callFunction(() => {
-        setShow(true);
-      })      
-      .start();
-  }}
-/> 
-{show&&<p style={{display:'flex'}}>
-  <Typewriter
-     options={{
-      delay:1, cursor:''
-    }}
-      onInit={(typewriter) => {
-        typewriter.typeString("<span id='a'>guest@fvckfiat.com</span>:<span id='b'>~</span><span id='c'>$</span>")     
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-  }}
-/> 
-<span class='cursor'><input type='text' class='datainput' onKeyDown={handleKeyDown} ref={commandInput}  /><i></i></span> </p> } 
+      <h1 className="glitch">Morse code !!!</h1>
+      <Typewriter
+        options={{
+          delay: 1,
+          cursor: "",
+        }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString(
+              "<span id='consoleText'>Welcome! Type 'help' to see list of commands. </span>"
+            )
+            .callFunction(() => {
+              setShow(true);
+            })
+            .start();
+        }}
+      />
+      {messages &&
+        messages.map((messages, index) => (
+          <>
+            <p style={{ display: "flex" }} key={index}>
+              <Typewriter
+                options={{
+                  delay: 1,
+                  cursor: "",
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString(
+                      "<span id='a'>guest@morseeee</span>:<span id='b'>~</span><span id='c'>$</span>"
+                    )
+                    .callFunction(() => {
+                      console.log("All strings were deleted");
+                    })
+                    .start();
+                }}
+              />
+              <span>
+                {messages.key}
+                <i></i>
+              </span>{" "}
+            </p>
+            <Typewriter
+              options={{
+                delay: 1,
+                cursor: "",
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(messages.message)
+                  .callFunction(() => {
+                    setAskCommand(true);
+                  })
+                  .start();
+              }}
+            />
+          </>
+        ))}
 
-{notFound && <> <Typewriter
-     options={{
-      delay:1, cursor:''
-    }}
-      onInit={(typewriter) => {
-      typewriter.typeString("Command not found. Type 'help' to see list of commands.")
-      .callFunction(() => {
-        setAskCommand(true);
-      })      
-      .start();
-  }}
-/>
-{askCommand&&<p style={{display:'flex'}}>
-  <Typewriter
-     options={{ 
-      delay:1, cursor:''
-    }}
-      onInit={(typewriter) => {
-        typewriter.typeString("<span id='a'>guest@fvckfiat.com</span>:<span id='b'>~</span><span id='c'>$</span>")     
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-  }}
-/> 
-<span class='cursor'><input type='text' class='datainput' onKeyDown={handleKeyDown} ref={commandInput}  /><i></i></span> </p> }
-</>
-}
+      {show && (
+        <p style={{ display: "flex" }}>
+          <Typewriter
+            options={{
+              delay: 1,
+              cursor: "",
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  "<span id='a'>guest@morse</span>:<span id='b'>~</span><span id='c'>$</span>"
+                )
+                .callFunction(() => {
+                  console.log("All strings were deleted");
+                })
+                .start();
+            }}
+          />
+          <span class="cursor">
+            <input
+              type="text"
+              class="datainput"
+              onKeyDown={(e) => handleKeyDown(e)}
+              ref={commandInput}
+            />
+            <i></i>
+          </span>{" "}
+        </p>
+      )}
 
-</div>
-      
-     
+      {tempMessage ? (
+        <Typewriter
+          options={{
+            delay: 1,
+            cursor: "",
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(tempMessage)
+              .callFunction(() => {
+                setAskCommand(true);
+              })
+              .start();
+          }}
+        />
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
 
